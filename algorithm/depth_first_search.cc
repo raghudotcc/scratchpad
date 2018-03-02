b5e037fa653aa578ed439ffe8a22b72cd46cc6d3
Raghu <raghu.94v@gmail.com>
2018-03-02 16:29:26 +0530

    Added dfs

--- algorithm/depth_first_search.cc
+++ algorithm/depth_first_search.cc
@@ -1,42 +1,43 @@
 /* Author@raghu
- * breadth first search class to recursively search a connected Graph breadth wise
+ * depth first search class to recursively search a connected Graph depth wise
  * PRE-CONDN: Must be a connected graph
  * LIMITATION: doesn't work on unconnected graph
- * RETURNS: NONE, you could make it return something
+ * RETURNS: NONE, you could make it return something, Although the graph holds the data you can use
  */
-struct Graph{
+struct Graph {
 private:
 	int current_vertex;
-	std::vector<vector<int>> edge;
+	std::vector<std::vector<int>> edge;
 public:
-	Graph(std::vector<std::vector<int>> edge) :  edge(edge) {}
+	Graph(std::vector<std::vector<int>> edge) : edge(edge) {}
 
 	void connect(int u, int v) 
-	{	
+	{
 		edge[u].push_back(v);
 	}
 
-	void bfs(std::queue<int>& Queue, bool *is_visited)
+	void dfs(std::queue<int>& Queue, bool *is_visited) 
 	{
-		while( !Queue.empty()) {
-			current_vertex = Queue.front(); 
-			//cout << current_vertex << " ";// print to play around and see the way it behaves
-			is_visited[current_vertex] = true;
-			for (auto reachable_vertex = 0; 
-				 reachable_vertex < edge[current_vertex].size(); 
-				 reachable_vertex++) 
-				if (is_visited[edge[current_vertex][reachable_vertex]] 
-						== false)
-					Queue.push(edge[current_vertex][reachable_vertex]);
+		if (!Queue.empty()) {
+			current_vertex = Queue.front();
 			Queue.pop();
-			bfs(Queue, is_visited);
+			if(is_visited[current_vertex] == false) {
+				is_visited[current_vertex] = true;
+				//cout << current_vertex << " "; // test me
+			}
+			for (auto reachable_vertex = 0; reachable_vertex < edge[current_vertex].size(); reachable_vertex++)
+				if (is_visited[edge[current_vertex][reachable_vertex]] 
+						== false) {
+					Queue.push(edge[current_vertex][reachable_vertex]);
+					dfs(Queue, is_visited);
+				}
 		}
+
 		return;
 	}
+
 };
 
-
- 
 /* 
  * Driver Stub to test
  * I used C style stub with commas for quick checking, Old habits. 
@@ -50,9 +51,8 @@ public:
  *		queue<int> Queue;
  *		bool visited[4] = {false};
  *
- *		Queue.push(2), graph.bfs(Queue, visited);
+ *		Queue.push(2), graph.dfs(Queue, visited);
  *		return 0;
  *	}
  *
  */
-
